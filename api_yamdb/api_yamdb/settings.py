828d18b551ed406c0fd00ecbac6d360193ae8200
vlad3069 <vlad9603@gmail.com>
2023-04-15 19:58:17 +0300

    fix_debug

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_KEY', 'some_key')
 
-DEBUG = os.getenv('DEBUG', default=False),
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = [
     'localhost',
